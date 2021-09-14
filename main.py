a4a948c38d55c30e0ccd0bf13994939850b969f1
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2021-09-14 10:48:14 +0530

    Adding Code

---
+++ main.py
@@ -0,0 +1,84 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+from pynput.keyboard import Controller
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+''' Declaring the necessary variables '''
+detector = HandDetector(detectionCon=0.8)
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+finalText = ""
+keyboard = Controller()
+
+
+''' Declaration of a function for the purpose of Button of keyboard '''
+
+
+class Button:
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+
+
+buttonList = []
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
+
+
+def drawall(self, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, button.text, (x + 20, y + 65),
+                    cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+        # out = img.copy()
+        # alpha = 0.5
+        # mask = shapes.astype(bool)
+        # out[mask] = cv2.addWeighted(img, alpha, shapes, 1 - alpha, 0)[mask]
+    return img
+
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bboxInfo = detector.findPosition(img)
+    img = drawall(img, buttonList)
+
+    if lmList:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+
+            ''' 8 is the tip of index finger.
+            Check the MEDIA PIPE doc for finding more... :) BHOO you rock '''
+            if x < lmList[8][0] < x + w and \
+                    y < lmList[8][1] < y + h:
+                cv2.rectangle(img, button.pos, (x + w, y + h),
+                              (175, 0, 175), cv2.FILLED)
+                cv2.putText(img, button.text, (x + 20, y + 65),
+                            cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                l, _, _ = detector.findDistance(8, 12, img, draw=False)
+                # _ was used if we ignore a value
+                print(l)
+
+                if l < 30:
+                    keyboard.press(button.text)
+                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, button.text, (x + 20, y + 65),
+                                cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                    finalText += button.text
+                    sleep(0.2)
+
+    cv2.rectangle(img, (100, 350), (700, 450), (175, 0, 175), cv2.FILLED)
+    cv2.putText(img, finalText, (60, 430), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
